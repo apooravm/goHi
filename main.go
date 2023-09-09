14e065d2a848ac153122916fe11ce844b4a3be59
Apoorav <medal.apoorav@gmail.com>
2023-09-09 12:21:02 +0530

    cowsay

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Bruh :(")
+
+	cow := `	\   ^__^
+	 \  (oo)\_______
+	    (__)\       )\/\
+	        ||----w |
+	        ||     ||
+    `
+
+	text := "	"
+	if len(os.Args) > 1 {
+		for _, arg := range os.Args[1:] {
+    			text += arg + " "
+    	}
+	} else {
+		text += "Hi :D"
+	}
+	text_top := "	"
+	text_bottom := "	"
+	for i := 0; i < len(text); i++ {
+		text_top += "_"
+		text_bottom += "-"
+	} 
+    fmt.Println(text_top)
+    fmt.Println(text)
+    fmt.Println(text_bottom)
+    fmt.Println(cow)
 }
